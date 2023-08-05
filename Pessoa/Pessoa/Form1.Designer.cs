d67582d6d78acf1089cd0a0645ef3de3d96a221a
Pietro Sbardellati <129914051+PiSbards@users.noreply.github.com>
2023-08-04 22:12:12 -0300

    Add files via upload

---
+++ Pessoa/Pessoa/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Pessoa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPessoa = new System.Windows.Forms.DataGridView();
+            this.btCadastrar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btEditar = new System.Windows.Forms.Button();
+            this.btExcluir = new System.Windows.Forms.Button();
+            this.btSair = new System.Windows.Forms.Button();
+            this.btLocalizar = new System.Windows.Forms.Button();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtCelular = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPessoa
+            // 
+            this.dgvPessoa.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPessoa.Location = new System.Drawing.Point(12, 211);
+            this.dgvPessoa.Name = "dgvPessoa";
+            this.dgvPessoa.Size = new System.Drawing.Size(431, 234);
+            this.dgvPessoa.TabIndex = 0;
+            // 
+            // btCadastrar
+            // 
+            this.btCadastrar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btCadastrar.FlatAppearance.BorderSize = 0;
+            this.btCadastrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCadastrar.ForeColor = System.Drawing.Color.White;
+            this.btCadastrar.Location = new System.Drawing.Point(328, 10);
+            this.btCadastrar.Name = "btCadastrar";
+            this.btCadastrar.Size = new System.Drawing.Size(115, 36);
+            this.btCadastrar.TabIndex = 1;
+            this.btCadastrar.Text = "CADASTRAR";
+            this.btCadastrar.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 26);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 3;
+            // 
+            // btEditar
+            // 
+            this.btEditar.BackColor = System.Drawing.Color.MidnightBlue;
+            this.btEditar.FlatAppearance.BorderSize = 0;
+            this.btEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btEditar.ForeColor = System.Drawing.Color.White;
+            this.btEditar.Location = new System.Drawing.Point(328, 64);
+            this.btEditar.Name = "btEditar";
+            this.btEditar.Size = new System.Drawing.Size(115, 36);
+            this.btEditar.TabIndex = 4;
+            this.btEditar.Text = "EDITAR";
+            this.btEditar.UseVisualStyleBackColor = false;
+            // 
+            // btExcluir
+            // 
+            this.btExcluir.BackColor = System.Drawing.Color.Purple;
+            this.btExcluir.FlatAppearance.BorderSize = 0;
+            this.btExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btExcluir.ForeColor = System.Drawing.Color.White;
+            this.btExcluir.Location = new System.Drawing.Point(328, 121);
+            this.btExcluir.Name = "btExcluir";
+            this.btExcluir.Size = new System.Drawing.Size(115, 36);
+            this.btExcluir.TabIndex = 5;
+            this.btExcluir.Text = "EXCLUIR";
+            this.btExcluir.UseVisualStyleBackColor = false;
+            // 
+            // btSair
+            // 
+            this.btSair.BackColor = System.Drawing.Color.Crimson;
+            this.btSair.FlatAppearance.BorderSize = 0;
+            this.btSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSair.ForeColor = System.Drawing.Color.White;
+            this.btSair.Location = new System.Drawing.Point(328, 169);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(115, 36);
+            this.btSair.TabIndex = 6;
+            this.btSair.Text = "SAIR";
+            this.btSair.UseVisualStyleBackColor = false;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
+            // btLocalizar
+            // 
+            this.btLocalizar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btLocalizar.FlatAppearance.BorderSize = 0;
+            this.btLocalizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLocalizar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btLocalizar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btLocalizar.Location = new System.Drawing.Point(134, 17);
+            this.btLocalizar.Name = "btLocalizar";
+            this.btLocalizar.Size = new System.Drawing.Size(115, 36);
+            this.btLocalizar.TabIndex = 7;
+            this.btLocalizar.Text = "LOCALIZAR";
+            this.btLocalizar.UseVisualStyleBackColor = false;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(15, 80);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(246, 20);
+            this.txtNome.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(15, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nome";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(18, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(207, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // txtCelular
+            // 
+            this.txtCelular.AutoSize = true;
+            this.txtCelular.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCelular.Location = new System.Drawing.Point(18, 121);
+            this.txtCelular.Name = "txtCelular";
+            this.txtCelular.Size = new System.Drawing.Size(53, 15);
+            this.txtCelular.TabIndex = 10;
+            this.txtCelular.Text = "Celular";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 457);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.txtCelular);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btLocalizar);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.btExcluir);
+            this.Controls.Add(this.btEditar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btCadastrar);
+            this.Controls.Add(this.dgvPessoa);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de Pessoa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPessoa)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPessoa;
+        private System.Windows.Forms.Button btCadastrar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btEditar;
+        private System.Windows.Forms.Button btExcluir;
+        private System.Windows.Forms.Button btSair;
+        private System.Windows.Forms.Button btLocalizar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label txtCelular;
+    }
+}
+
---
+++ Pessoa/Pessoa/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pessoa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btSair_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pessoa pessoa = new Pessoa();
+            List<Pessoa> pessoas = pessoa.listaPessoa();
+            dgvPessoa.DataSource = pessoas;
+            btEditar.Enabled = false;
+            btExcluir.Enabled = false;
+        }
+    }
+}
